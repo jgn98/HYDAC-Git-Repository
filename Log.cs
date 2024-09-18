e690a34399420a327741f5798cc6af7288f4d4b1
Simon From <simon.bulder@gmail.com>
2024-09-18 14:53:54 +0200

    Log method created

--- Log.cs
+++ Log.cs
@@ -1,6 +1,19 @@
+using System.Security.Principal;
+
 namespace HYDAC_Projekt;
 
 public class Log
 {
-    
+    public void GuestCheckIn()
+    {
+        string HydacGuests = @"C:\Users\simon\Desktop\Hydac Projekt\HydacGuests.txt";
+        string HydacLog = @"C:\Users\simon\Desktop\Hydac Projekt\HydacLog.txt";
+        Guest GID = new Guest();
+
+        DateTime Now = DateTime.Now;
+        string Time = DateTime.Now.ToString();
+        string[] GuestCheckIn = { Time };
+        string IdString = Convert.ToString(GID.GuestID());
+        File.AppendAllLines(HydacLog, new string[] { IdString, Time });
+    }
 }
